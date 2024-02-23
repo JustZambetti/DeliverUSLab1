f79da70796c1a550df7ac7b7eabb52ca3dfca16d
Zambetti Giorgio <giorgio.zambo2@gmail.com>
2024-02-23 11:53:25 +0100

    lab1

--- DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
+++ DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
@@ -1,17 +1,98 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Restaurants', {
+    await queryInterface.createTable("Restaurants", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
-      }
-      // TODO: Include the rest of the fields of the Restaurants table
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      averageServiceMinutes: {
+        allowNull: true,
+        type: Sequelize.DOUBLE,
 
-    })
+      },
+      description: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      address: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      postalCode: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      url: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      restaurantCategoryId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: "RestaurantCategories",
+          },
+          key: "id",
+        },
+      },
+      shippingCosts: {
+        allowNull: false,
+        type: Sequelize.DOUBLE,
+      },
+      email: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      logo: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      phone: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date(),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date(),
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: {
+            tableName: "Users",
+          },
+          key: "id",
+        },
+      },
+      heroImage: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.ENUM(
+          'online',
+          'offline',
+          'closed',
+          'temporarily closed'
+        ),
+      },
+    });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Restaurants')
-  }
-}
+    await queryInterface.dropTable('Restaurants');
+  },
+};
--- DeliverUS-Backend/src/models/Restaurant.js
+++ DeliverUS-Backend/src/models/Restaurant.js
@@ -28,7 +28,82 @@ const loadModel = (sequelize, DataTypes) => {
   }
   Restaurant.init({
     // TODO: Include the rest of the properties of the Restaurant model
-
+    name: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    description: {
+      allowNull: true,
+      type: DataTypes.STRING
+    },
+    averageServiceMinutes: {
+      allowNull: true,
+      type: DataTypes.DOUBLE,
+    },
+    address: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    postalCode: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    url: {
+      allowNull: true,
+      type: DataTypes.STRING
+    },
+    restaurantCategoryId: {
+      type: DataTypes.INTEGER
+    },
+    shippingCosts: {
+      allowNull: false,
+      type: DataTypes.DOUBLE
+    },
+    email: {
+      allowNull: true,
+      type: DataTypes.STRING
+    },
+    logo: {
+      allowNull: true,
+      type: DataTypes.STRING
+    },
+    phone: {
+      allowNull: true,
+      type: DataTypes.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue: new Date()
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue: new Date()
+    },
+    userId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      references: {
+        model: {
+          tableName: 'Users'
+        },
+        key: 'id'
+      }
+    },
+    heroImage: {
+      allowNull: true,
+      type: DataTypes.STRING,
+    },
+    status: {
+      allowNull: false,
+      type: DataTypes.ENUM(
+          'online',
+          'offline',
+          'closed',
+          'temporarily closed'
+      ),
+    },
   }, {
     sequelize,
     modelName: 'Restaurant'
